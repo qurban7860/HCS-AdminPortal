050fff895d67b80d3f5167c01c80f93a0c5cb138
Muzna Farooq <muznafarooq21@gmail.com>
2023-02-21 15:56:40 +0500

    I just comment out some things

--- src/pages/machine/SupplierDetail.js
+++ src/pages/machine/SupplierDetail.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useLayoutEffect } from 'react'
 
 function SupplierDetail() {
-        useLayoutEffect(()=>{
-            dispatch(getSuppliers(id));
-          },[dispatch, id]);
+        // useLayoutEffect(()=>{
+        //     dispatch(getSuppliers(id));
+        //   },[dispatch, id]);
   return (
     <div>
     <h1>hello</h1>
