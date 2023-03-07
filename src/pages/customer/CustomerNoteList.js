6b332a0a977c22fb9d093ca6a6fc4a71cead04a5
Ali-haxxan <ali.haxxan@live.co.uk>
2023-03-07 13:10:07 +0500

    Bug fix on customer page

---
+++ src/pages/customer/CustomerNoteList.js
@@ -0,0 +1,368 @@
+import { Helmet } from 'react-helmet-async';
+import { paramCase } from 'change-case';
+import { useState, useEffect, useLayoutEffect } from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+// @mui
+import {
+  Stack,
+  Card,
+  Item,
+  Grid,
+  Box,
+  Table,
+  Button,
+  Tooltip,
+  TableBody,
+  Container,
+  IconButton,
+  TableContainer,
+  Typography,
+  Accordion, 
+  AccordionSummary,
+  AccordionDetails
+} from '@mui/material';
+import { fDate,fDateTime } from '../../utils/formatTime';
+
+// redux
+import { useDispatch, useSelector } from '../../redux/store';
+// routes
+import { PATH_DASHBOARD } from '../../routes/paths';
+// components
+import { useSnackbar } from '../../components/snackbar';
+import { useSettingsContext } from '../../components/settings';
+import {
+  useTable,
+  getComparator,
+  emptyRows,
+  TableNoData,
+  TableSkeleton,
+  TableEmptyRows,
+  TableHeadCustom,
+  TableSelectedAction, 
+  TablePaginationCustom,
+} from '../../components/table';
+import Iconify from '../../components/iconify';
+import Scrollbar from '../../components/scrollbar';
+import CustomBreadcrumbs from '../../components/custom-breadcrumbs';
+import EmptyContent from '../../components/empty-content';
+
+import ConfirmDialog from '../../components/confirm-dialog';
+// sections
+import NotesViewForm from './note/NotesViewForm';
+import NoteStatistics from './note/NoteStatistics';
+import NoteEditForm from './note/NoteEditForm';
+import NoteAddForm from './note/NoteAddForm';
+
+
+
+import { getNotes, deleteNote, getNote ,updateNote,setNoteFormVisibility} from '../../redux/slices/note';
+import { getSites } from '../../redux/slices/site';
+import { getContacts } from '../../redux/slices/contact';
+
+
+
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  { id: 'note', label: 'Note', align: "left"},
+  { id: 'isDisabled', label: 'Disabled', align: "left"},
+  { id: 'created_at', label: 'Created At', align: "left"},
+  { id: 'action', label: 'Actions', align: "left"},
+
+];
+
+const STATUS_OPTIONS = [
+  // { id: '1', value: 'Order Received' },
+  // { id: '2', value: 'In Progress' },
+  // { id: '3', value: 'Ready For Transport' },
+  // { id: '4', value: 'In Freight' },
+  // { id: '5', value: 'Deployed' },
+  // { id: '6', value: 'Archived' },
+];
+
+// const STATUS_OPTIONS = [
+//   { value: 'all_notes', label: 'All Note' },
+//   { value: 'deployable', label: 'All Deployable' },
+//   { value: 'pending', label: 'All Pending' },
+//   { value: 'archived', label: 'All Archived' },
+//   { value: 'undeployable', label: 'All Undeployable' }
+// ];
+
+// ----------------------------------------------------------------------
+
+export default function NoteList() {
+  const {
+    dense,
+    page,
+    order,
+    orderBy,
+    rowsPerPage,
+    setPage,
+    //
+    selected,
+    setSelected,
+    onSelectRow,
+    onSelectAllRows,
+    //
+    onSort,
+    onChangeDense,
+    onChangePage,
+    onChangeRowsPerPage,
+  } = useTable({
+    defaultOrderBy: 'createdAt',
+  });
+
+  const dispatch = useDispatch();
+
+  const { themeStretch } = useSettingsContext();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const navigate = useNavigate();
+
+  const [filterName, setFilterName] = useState('');
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const [expanded, setExpanded] = useState(false);
+  const [filterStatus, setFilterStatus] = useState([]);
+
+  const [openConfirm, setOpenConfirm] = useState(false);
+
+  
+  const handleAccordianClick = (accordianIndex) => {
+   if(accordianIndex === activeIndex ){
+    setActiveIndex(null)
+   }else{
+    setActiveIndex(accordianIndex)
+   }
+  };
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+    console.log("Expended : ",expanded)
+  };
+  
+
+  const { notes, isLoading, error, initial, responseMessage ,noteEditFormVisibility, formVisibility} = useSelector((state) => state.note);
+  const { customer } = useSelector((state) => state.customer);
+  const [checked, setChecked] = useState(false);
+// console.log("noteEditFormVisibility: " , noteEditFormVisibility)
+
+  useLayoutEffect(() => {
+    if(!formVisibility && !noteEditFormVisibility){
+    dispatch(getNotes(customer._id));
+    dispatch(getContacts(customer._id));
+    dispatch(getSites(customer._id));
+    }
+  }, [dispatch, customer._id, noteEditFormVisibility, formVisibility ]);
+// console.log(customer._id)
+  useEffect(() => {
+    if (initial) {
+      if (notes && !error) {
+        enqueueSnackbar(responseMessage);
+      } else {
+        enqueueSnackbar(error, { variant: `error` });
+      }
+      setTableData(notes);
+    }
+  }, [notes, error,checked, customer, responseMessage, enqueueSnackbar, initial]);
+
+  const dataFiltered = applyFilter({
+    inputData: tableData,
+    comparator: getComparator(order, orderBy),
+    filterName,
+    filterStatus,
+  });
+
+
+//  -----------------------------------------------------------------------
+
+
+  const dataInPage = dataFiltered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
+//   const denseHeight = dense ? 60 : 80;
+
+//   const isFiltered = filterName !== '' || !!filterStatus.length;
+
+  const isNotFound = (!dataFiltered.length && !!filterName) || (!isLoading && !dataFiltered.length);
+
+//   const handleOpenConfirm = () => {
+//     setOpenConfirm(true);
+//   };
+
+  const handleCloseConfirm = () => {
+    setOpenConfirm(false);
+  };
+
+//   const handleFilterName = (event) => {
+//     setPage(0);
+//     setFilterName(event.target.value);
+//   };
+
+//   const handleFilterStatus = (event) => {
+//     setPage(0);
+//     setFilterStatus(event.target.value);
+//   };
+
+// const showHide = ()=>{
+//   if(this.state.showHide === 'hide') {
+//       this.setState({
+//           showHide: 'show'
+//       ))};
+//   } else {
+//       this.setState({
+//           showHide: 'hide'
+//       });
+//   }
+
+// }
+const toggleChecked = () => 
+    {
+      setChecked(value => !value);
+      dispatch(setNoteFormVisibility(!formVisibility));
+    
+    };
+
+  const handleDeleteRow = async (id) => {
+    try {
+      // console.log(id);
+      await dispatch(deleteNote(id));
+      dispatch(getNotes());
+      setSelected([]);
+
+      if (page > 0) {
+        if (dataInPage.length < 2) {
+          setPage(page - 1);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDeleteRows = (selectedRows) => {
+    const deleteRows = tableData.filter((row) => !selectedRows.includes(row._id));
+    setSelected([]);
+    setTableData(deleteRows);
+
+    if (page > 0) {
+      if (selectedRows.length === dataInPage.length) {
+        setPage(page - 1);
+      } else if (selectedRows.length === dataFiltered.length) {
+        setPage(0);
+      } else if (selectedRows.length > dataInPage.length) {
+        const newPage = Math.ceil((tableData.length - selectedRows.length) / rowsPerPage) - 1;
+        setPage(newPage);
+      }
+    }
+  };
+
+//   const handleEditRow = (id) => {
+//     console.log(id);
+//     navigate(PATH_DASHBOARD.note.edit(id));
+//   };
+
+//   const handleViewRow = (id) => {
+//     navigate(PATH_DASHBOARD.note.view(id));
+//   };
+
+//   const handleResetFilter = () => {
+//     setFilterName('');
+//     setFilterStatus([]);
+//   };
+// ------------------------------------------------------------------------------------
+
+
+  return (
+    <>
+      <Helmet>
+        <title> Note: List | Machine ERP </title>
+      </Helmet>
+      <Container maxWidth={themeStretch ? false : 'lg'}>
+        {!noteEditFormVisibility && 
+            <Stack alignItems="flex-end" sx={{ mt: 3, padding: 2 }}>
+                <Button
+                    onClick={toggleChecked}
+                    variant="contained"
+                    startIcon={!formVisibility ? <Iconify icon="eva:plus-fill" /> : <Iconify icon="eva:minus-fill" />}
+                    >
+                    New Note 
+                </Button>
+            </Stack>
+        }
+        <Card>
+          {noteEditFormVisibility && <NoteEditForm/> }
+          {formVisibility && !noteEditFormVisibility && <NoteAddForm/>}
+          {!formVisibility && !noteEditFormVisibility && notes.map((note, index) => (
+            <Accordion key={note._id} expanded={expanded === index} onChange={handleChange(index)}  onClick={()=>handleAccordianClick(index)} >
+              <AccordionSummary   expandIcon={<Iconify icon="eva:arrow-ios-downward-fill" />} >
+            { index !==  activeIndex ? 
+              <Grid container spacing={0}>
+                <Grid item xs={12} sm={4} >
+                  <Typography variant="body2" >
+                    {fDateTime(note.createdAt)}  
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                  <Typography variant="body2" >
+                      {note.note.length > 100 ? note.note.substring(0, 100) :note.note}
+                      {note.note.length > 100 ? "..." :null}
+                  </Typography>
+                </Grid>
+              </Grid>
+            : null }
+              </AccordionSummary>
+              <AccordionDetails>
+                <NotesViewForm currentNote={note} />
+              </AccordionDetails>
+            </Accordion>
+          ))} 
+          {isNotFound && <EmptyContent title="No Data"/>}
+        </Card>
+      </Container>
+        <ConfirmDialog
+          open={openConfirm}
+          onClose={handleCloseConfirm}
+          title="Delete"
+          content={
+            <>
+              Are you sure want to delete <strong> {selected.length} </strong> items?
+            </>
+          }
+          action={
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => {
+                handleDeleteRows(selected);
+                handleCloseConfirm();
+              }}
+            >
+              Delete
+            </Button>
+          }
+        />
+    </>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+function applyFilter({ inputData, comparator, filterName, filterStatus }) {
+  const stabilizedThis = inputData.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  inputData = stabilizedThis.map((el) => el[0]);
+  if (filterName) {
+    inputData = inputData.filter(
+      (note) => note.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
+    );
+  }
+  if (filterStatus.length) {
+    inputData = inputData.filter((note) => filterStatus.includes(note.status));
+  }
+  return inputData;
+}
