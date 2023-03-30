fbd345481c11bef13e56c5a4fa8abaeb5e0c7328
Muzna Farooq <muznafarooq21@gmail.com>
2023-03-30 10:24:38 +0500

    name change

--- src/pages/machine/Parameters/ParameterEditForm.js
+++ src/pages/machine/Parameters/ParameterEditForm.js
@@ -15,7 +15,7 @@ import { TextField, Autocomplete, Box, Card, Container, Grid, Stack, Typography,
 // global
 
 // slice
-import { updateTechparam, getTechparam } from '../../../redux/slices/products/machinetechparam';
+import { updateTechparam, getTechparam } from '../../../redux/slices/products/machineTechParam';
 
 import { useSettingsContext } from '../../../components/settings';
 import {CONFIG} from '../../../config-global';
