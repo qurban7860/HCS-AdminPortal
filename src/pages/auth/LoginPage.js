f98c333726138a8d9f3d0181566c745b26d64b80
Ali Hassan <a.hassan@terminustech.com>
2023-05-02 11:00:44 +0500

    title commented on user login page

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -9,10 +9,10 @@ import { CONFIG } from '../../config-global';
 export default function LoginPage() {
   return (
     <>
-      <Helmet>
+      {/* <Helmet>
         <meta charSet="utf-8" />
         <title> Login | {process.env.REACT_APP_TITLE}</title>
-      </Helmet>
+      </Helmet> */}
       <Login />
     </>
   );
