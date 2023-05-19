28b8c0166d82eeb34a5bb3e790d75d8bf0dbcd15
awais <awais@terminustech.com>
2023-05-19 19:48:53 +0500

    trim condition improved

--- src/pages/customer/CustomerMachineList.js
+++ src/pages/customer/CustomerMachineList.js
@@ -194,7 +194,11 @@ export default function CustomerContactList() {
                         md={6}
                         display={{ sm: 'none', md: 'none', lg: 'block' }}
                       >
- 
+                        {customerMachine?.instalationSite?.address?.city?.city.trim() ? customerMachine?.instalationSite?.address?.city : ""}
+                        {customerMachine?.instalationSite?.address?.city?.city.trim() !== "" && customerMachine?.instalationSite?.address?.region?.region.trim() !== "" && ", "}
+                        {customerMachine?.instalationSite?.address?.region?.region.trim() ? customerMachine?.instalationSite?.address?.region : ""}
+                        {customerMachine?.instalationSite?.address?.country?.country.trim() !== "" && customerMachine?.instalationSite?.address?.region?.region.trim() !== "" && ", "}
+                        {customerMachine?.instalationSite?.address?.country?.country.trim() ? customerMachine?.instalationSite?.address?.country : ""}
                       </Grid>
                     </Grid>
                   ) : null}
