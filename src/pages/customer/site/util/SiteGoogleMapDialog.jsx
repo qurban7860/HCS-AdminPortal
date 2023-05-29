9a50d694785107840656d4dcbdfbc7e74d46da95
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-05-29 17:10:43 +1200

    pre -google map dialog

---
+++ src/pages/customer/site/util/SiteGoogleMapDialog.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import GoogleMaps from '../../../../assets/GoogleMaps';
+
+function SiteGoogleMapDialog() {
+  return <div>SiteGoogleMapDialog</div>;
+}
+
+export default SiteGoogleMapDialog;
