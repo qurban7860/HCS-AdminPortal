6b934de83284e9523f80ca2ebbb43a861efa28ba
Ali Hassan <a.hassan@terminustech.com>
2023-06-06 09:48:40 +0500

    customer Access configured on document type and category list

--- src/pages/document/documentType/DocumentTypeList.js
+++ src/pages/document/documentType/DocumentTypeList.js
@@ -49,6 +49,7 @@ import { fDate } from '../../../utils/formatTime';
 
 const TABLE_HEAD = [
   { id: 'name', label: 'Document Name', align: 'left' },
+  { id: 'customerAccess', label: 'Customer Access', align: 'center' },
   { id: 'active', label: 'Active', align: 'center' },
   { id: 'created_at', label: 'Created At', align: 'right' },
 
