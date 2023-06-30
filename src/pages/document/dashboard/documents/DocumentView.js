90c1beeabbde3e45f16e72da3d5545c56780eae2
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-06-30 15:42:45 +1200

    worked on documentview

--- src/pages/document/dashboard/documents/DocumentView.js
+++ src/pages/document/dashboard/documents/DocumentView.js
@@ -2,16 +2,15 @@ import { Helmet } from 'react-helmet-async';
 import { useEffect, useLayoutEffect, useState } from 'react';
 import { Link as RouterLink, useNavigate, useParams } from 'react-router-dom';
 // @mui
-import { Card, Container } from '@mui/material';
+import { Container } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 // routes
 import { PATH_MACHINE } from '../../../../routes/paths';
 // redux
-
 import { getDocumentCategory } from '../../../../redux/slices/document/documentCategory';
-// sections
-import { Cover } from '../../../components/Cover';
+// components
 import DocumentViewForm from './DocumentViewForm';
+import DocumentCover from '../../../components/DocumentForms/DocumentCover';
 /* eslint-disable */
 
 // ----------------------------------------------------------------------
@@ -28,15 +27,7 @@ export default function DocumentView() {
   return (
     <>
       <Container maxWidth={false}>
-        <Card
-          sx={{
-            mb: 3,
-            height: 160,
-            position: 'relative',
-          }}
-        >
-          <Cover name={documentCategory?.name} />
-        </Card>
+        <DocumentCover content={documentCategory?.name} />
         <DocumentViewForm />
       </Container>
     </>
