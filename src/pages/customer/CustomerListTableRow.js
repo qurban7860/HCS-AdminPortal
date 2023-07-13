e7ca23ab408bb4b68a5d98b10572768eecf91507
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-07-13 19:01:04 +1200

    merge

--- src/pages/customer/CustomerListTableRow.js
+++ src/pages/customer/CustomerListTableRow.js
@@ -2,12 +2,29 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { sentenceCase } from 'change-case';
 // @mui
-import { Switch, TableRow, TableCell } from '@mui/material';
+import {
+  Switch,
+  Stack,
+  Button,
+  TableRow,
+  Checkbox,
+  MenuItem,
+  TableCell,
+  IconButton,
+  Link,
+  Chip,
+} from '@mui/material';
 // utils
+import { borderRadius, styled } from '@mui/system';
 import { fDate } from '../../utils/formatTime';
+import { fCurrency } from '../../utils/formatNumber';
 // components
 import Iconify from '../../components/iconify';
+import MenuPopover from '../../components/menu-popover';
+import ConfirmDialog from '../../components/confirm-dialog';
+import Label from '../../components/label';
 import LinkTableCellWithIcon from '../components/ListTableTools/LinkTableCellWithIcon';
+import { useSelector } from '../../redux/store';
 import { StyledTableRow } from '../../theme/styles/default-styles';
 
 // ----------------------------------------------------------------------
@@ -21,6 +38,15 @@ CustomerListTableRow.propTypes = {
   onSelectRow: PropTypes.func,
   onDeleteRow: PropTypes.func,
 };
+
+// const StyledTableRow = styled(TableRow)(({ theme }) => ({
+//   '&:nth-of-type(odd)': {
+//     backgroundColor: 'white',
+//   },
+//   '&:nth-of-type(even)': {
+//     backgroundColor: '#f4f6f866',
+//   },
+// }));
 
 export default function CustomerListTableRow({
   row,
@@ -40,7 +66,9 @@ export default function CustomerListTableRow({
     address.push(mainSite?.address?.country);
   }
   const [openConfirm, setOpenConfirm] = useState(false);
+
   const [openPopover, setOpenPopover] = useState(null);
+
   const userId = localStorage.getItem('userId');
 
   const handleOpenConfirm = () => {
@@ -58,30 +86,91 @@ export default function CustomerListTableRow({
   const handleClosePopover = () => {
     setOpenPopover(null);
   };
+
   return (
-    <StyledTableRow hover selected={selected}>
-      <TableCell align="right">
-        {type === 'SP' && (
-          <Iconify icon="octicon:star-24" sx={{ color: 'text.disabled', mr: -2 }} width="15px" />
-        )}
-      </TableCell>
-      <LinkTableCellWithIcon
-        align="left"
-        onClick={onViewRow}
-        param={name}
-        isVerified={verifications?.length > 0}
-      />
-      <TableCell>{tradingName}</TableCell>
-      <TableCell>
-        {Object.values(address ?? {})
-          .map((value) => (typeof value === 'string' ? value.trim() : ''))
-          .filter((value) => value !== '')
-          .join(', ')}
-      </TableCell>
-      <TableCell align="center">
-        <Switch checked={isActive} disabled size="small" />{' '}
-      </TableCell>
-      <TableCell>{fDate(createdAt)}</TableCell>
-    </StyledTableRow>
+    <>
+      <StyledTableRow hover selected={selected}>
+        {/* <TableCell padding="checkbox">
+          <Checkbox checked={selected} onClick={onSelectRow} />
+        </TableCell> */}
+        {/* <Iconify icon="octicon:package-dependents-16" sx={{ color: 'text.disabled' }} /> */}
+        <TableCell align="right">
+          {type === 'SP' ? (
+            <Iconify icon="octicon:star-24" sx={{ color: 'text.disabled', mr: -2 }} width="15px" />
+          ) : (
+            ''
+          )}
+        </TableCell>
+        <LinkTableCellWithIcon
+          align="left"
+          onClick={onViewRow}
+          param={name}
+          isVerified={verifications?.length > 0}
+        />
+        <TableCell>
+          {tradingName.map((value) =>
+            typeof value === 'string'
+              ? value.trim() !== '' && <Chip label={value} sx={{ mx: 0.3 }} />
+              : ''
+          )}
+        </TableCell>
+        <TableCell>
+          {Object.values(address ?? {})
+            .map((value) => (typeof value === 'string' ? value.trim() : ''))
+            .filter((value) => value !== '')
+            .join(', ')}
+        </TableCell>
+        <TableCell align="center">
+          {' '}
+          <Switch checked={isActive} disabled size="small" />{' '}
+        </TableCell>
+        <TableCell>{fDate(createdAt)}</TableCell>
+        {/* <TableCell align="center">
+          <IconButton color={openPopover ? 'primary' : 'default'} onClick={handleOpenPopover}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+        </TableCell>   */}
+      </StyledTableRow>
+
+      {/* <MenuPopover
+        open={openPopover}
+        onClose={handleClosePopover}
+        arrow="right-top"
+        sx={{ width: 140 }}
+      >
+        <MenuItem
+          onClick={() => {
+            handleOpenConfirm();
+            handleClosePopover();
+          }}
+          sx={{ color: 'error.main' }}
+        >
+          <Iconify icon="eva:trash-2-outline" />
+          Delete
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            onEditRow();
+            handleClosePopover();
+          }}
+        >
+          <Iconify icon="eva:edit-fill" />
+          Edit
+        </MenuItem>
+      </MenuPopover> */}
+
+      {/* <ConfirmDialog
+        open={openConfirm}
+        onClose={handleCloseConfirm}
+        title="Delete"
+        content="Are you sure want to delete?"
+        action={
+          <Button variant="contained" color="error" onClick={onDeleteRow}>
+            Delete
+          </Button>
+        }
+      /> */}
+    </>
   );
 }
