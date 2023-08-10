0f50cb44dc957c06f8eec98ade7ce6b75ac23a1e
Muzna Farooq <muzna@terminustech.com>
2023-08-10 15:21:47 +0500

    changes

--- src/pages/user/SecurityUserAddForm.js
+++ src/pages/user/SecurityUserAddForm.js
@@ -587,7 +587,7 @@ export default function SecurityUserAddForm({ isEdit = false, currentUser }) {
                     }}
                   >
                     {' '}
-                    MultiFactorAuthentication
+                    Multi-Factor Authentication
                   </Typography>
                 }
               />
