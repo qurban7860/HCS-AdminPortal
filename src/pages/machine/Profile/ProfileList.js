e56d338a38548e60ec2e898187990555617a666a
Muhammad Zeeshan Aslam <zamrana6768@gmail.com>
2023-09-18 14:48:55 +0500

    profile

--- src/pages/machine/Profile/ProfileList.js
+++ src/pages/machine/Profile/ProfileList.js
@@ -42,7 +42,7 @@ export default function ProfileList() {
     setPage,
     onSort,
   } = useTable({
-    defaultOrderBy: 'deafultName',
+    defaultOrderBy: 'createdAt',
   });
 
   const dispatch = useDispatch();
