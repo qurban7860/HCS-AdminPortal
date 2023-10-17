7809f8424a93ac8e61f4c2ce28629bda53b8958c
Ali Hassan <a.hassan@terminustech.com>
2023-10-17 16:50:36 +0500

    check item list revised on add/edit

--- src/pages/machine/MachineServiceRecord/CollapsibleCheckedItemInputRow.js
+++ src/pages/machine/MachineServiceRecord/CollapsibleCheckedItemInputRow.js
@@ -22,25 +22,49 @@ const CollapsibleCheckedItemInputRow = ({ row, index, checkParamList, setValue,
                 <Table size="small" aria-label="purchases">
                   <TableBody>
                     {row?.paramList.map((childRow,childIndex) => (
+                      <>
                       <TableRow key={childRow._id} 
                           sx={{ ":hover": { backgroundColor: "#dbdbdb66" } }}
                       >
-                        <TableCell component="th" size='small'>
-                          <b>{`${childIndex+1}). `}</b>
-                          {`${childRow.name}`}
-                        </TableCell>
-                        <TableCell align='right' >
-                                <CommentsInput index={index} childIndex={childIndex} 
-                                  childRow={childRow}
-                                  checkParamList={checkParamList} 
-                                  handleChangeCheckItemListDate={handleChangeCheckItemListDate}
-                                  handleChangeCheckItemListValue={handleChangeCheckItemListValue}
-                                  handleChangeCheckItemListStatus={handleChangeCheckItemListStatus}
-                                  handleChangeCheckItemListComment={handleChangeCheckItemListComment}
-                                  handleChangeCheckItemListCheckBoxValue={handleChangeCheckItemListCheckBoxValue}
-                                />
-                        </TableCell>
+                      <Grid display='flex' flexDirection='column'>
+                        <Grid  sx={{display: {sm: 'flex',xs:'block'}, justifyContent:'space-between', }}>
+                          <Typography variant='body2' size='small' sx={{ my:'auto'}} >
+                            <b>{`${childIndex+1}). `}</b>
+                            {`${childRow.name}`}
+                          </Typography>
+                          <Grid align='right' sx={{ ml: 'auto'}} >
+                                  <CommentsInput index={index} childIndex={childIndex} 
+                                    childRow={childRow}
+                                    checkParamList={checkParamList} 
+                                    handleChangeCheckItemListDate={handleChangeCheckItemListDate}
+                                    handleChangeCheckItemListValue={handleChangeCheckItemListValue}
+                                    handleChangeCheckItemListStatus={handleChangeCheckItemListStatus}
+                                    handleChangeCheckItemListComment={handleChangeCheckItemListComment}
+                                    handleChangeCheckItemListCheckBoxValue={handleChangeCheckItemListCheckBoxValue}
+                                  />
+                          </Grid>
+                        </Grid>
+                        <Grid sx={{mx: {sm: 0.6, } }} >
+                          <TextField 
+                              // fullWidth
+                              type="text"
+                              label="Comment" 
+                              name="comment"
+                              onChange={(e) => handleChangeCheckItemListComment(index, childIndex, e.target.value)}
+                              size="small" sx={{ m: 0.3, width: '100%', }} 
+                              // pr:{md:4},width: {md: 470 } 
+                              value={checkParamList[index]?.paramList[childIndex]?.comments}
+                              minRows={1} multiline
+                              InputProps={{ inputProps: { maxLength: 500 }, 
+                              // style: { fontSize: '14px', height: 30 }
+                              }}
+                              // InputLabelProps={{ style: {  fontSize: '14px', top: '-4px' } }}
+                          />
+                        </Grid>
+                      </Grid>
                       </TableRow>
+                                
+                                </>
                     ))}
                   </TableBody>
                 </Table>
