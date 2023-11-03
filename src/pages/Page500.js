8c30f5c5ce4a0bbc510efdcfe60f3ab4a7a5b78d
Muhammad Zeeshan Aslam <zamrana6768@gmail.com>
2023-11-03 17:47:55 +0500

    fixed

--- src/pages/Page500.js
+++ src/pages/Page500.js
@@ -1,5 +1,6 @@
 // @mui
 import { Button, Typography, Grid } from '@mui/material';
+import { useMemo } from 'react';
 // components
 import { MotionContainer } from '../components/animate';
 import Logo from '../components/logo';
@@ -7,12 +8,23 @@ import Logo from '../components/logo';
 // ----------------------------------------------------------------------
 
 export default function Page500() {
+
+  const configurations = JSON.parse(localStorage.getItem('configurations'));
+  const content = configurations?.find((config) => (config.type === 'AUTH' && config.name === '500'));
+
+  const defaultValues = useMemo(
+    () => ({
+      title: content?.value || "Ooppps.. an error has occured",
+      message: content?.notes || "We're committed to delivering a seamless and delightful user experience. We apologies for the inconvenience, Thank you for your understanding, and stay tuned for updates. We'll be back before you know it.",
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
   return (
     <MotionContainer>
       <Grid sx={{ width: '900px', margin: 'auto', textAlign: 'center', pt: 10 }}>
-        <Typography variant="p" sx={{ color: 'text.secondary' }} paragraph>
-          Ooppps.. an error has occured
-        </Typography>
+        <Typography variant="p" sx={{ color: 'text.secondary' }} paragraph>{defaultValues.title}</Typography>
         <Logo
           sx={{
             width: '60%',
@@ -28,9 +40,7 @@ export default function Page500() {
           sx={{ color: '#c9c9c9', fontSize: 16, p: 19, pt: 0, pb: 2 }}
           paragraph
         >
-          We&apos;re committed to delivering a seamless and delightful user experience. We apologies
-          for the inconvenience, Thank you for your understanding, and stay tuned for updates.
-          We&apos;ll be back before you know it.
+          {defaultValues.message}
         </Typography>
         <Button onClick={() => window.open('/', '_self')} size="large" variant="contained">
           Go Back
