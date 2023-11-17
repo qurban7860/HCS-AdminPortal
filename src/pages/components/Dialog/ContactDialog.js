e4aa6f40e5f1a3ab16f2985c6457c5a48011ab8a
Muhammad Zeeshan Aslam <zamrana6768@gmail.com>
2023-11-17 17:49:52 +0500

    Skelton Implementation on Dialogs
    Customer Dialog
    Machine Dialog

--- src/pages/components/Dialog/ContactDialog.js
+++ src/pages/components/Dialog/ContactDialog.js
@@ -6,10 +6,11 @@ import DialogLabel from './DialogLabel';
 import ViewFormField from '../ViewForms/ViewFormField';
 import FormLabel from '../DocumentForms/FormLabel';
 import DialogLink from './DialogLink';
+import ViewFormFieldWithSkelton from '../ViewForms/ViewFormFieldWithSkelton';
 
 function ContactDialog() {
     const dispatch = useDispatch();
-    const { contact, contactDialog } = useSelector((state) => state.contact);
+    const { contact, contactDialog, isLoading} = useSelector((state) => state.contact);
     const handleConttactDialog = ()=>{ dispatch(setContactDialog(false)) }
   return (
     <Dialog
@@ -21,57 +22,57 @@ function ContactDialog() {
       >
         <DialogLabel content="Contact" onClick={handleConttactDialog} />
         <DialogContent dividers>
-        <Grid container sx={{ px: 2, py: 2 }}>
-          <ViewFormField
+        <Grid container >
+          <ViewFormFieldWithSkelton isLoading={isLoading}
             sm={6}
             heading="First Name"
             param={contact?.firstName && contact?.firstName}
           />
-          <ViewFormField
+          <ViewFormFieldWithSkelton isLoading={isLoading}
             sm={6}
             heading="Last Name"
             param={contact?.lastName && contact?.lastName}
           />
-          <ViewFormField sm={6} heading="Title" param={contact?.title && contact?.title} />
-          <ViewFormField
+          <ViewFormFieldWithSkelton isLoading={isLoading}  sm={6} heading="Title" param={contact?.title && contact?.title} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} 
             sm={6}
             heading="Contact Types"
             param={contact?.contactTypes && contact?.contactTypes.toString()}
           />
-          <ViewFormField sm={6} heading="Phone" param={contact?.phone && contact?.phone} />
-          <ViewFormField sm={6} heading="Email" param={contact?.email && contact?.email} />
+          <ViewFormFieldWithSkelton isLoading={isLoading}  sm={6} heading="Phone" param={contact?.phone && contact?.phone} />
+          <ViewFormFieldWithSkelton isLoading={isLoading}  sm={6} heading="Email" param={contact?.email && contact?.email} />
           <FormLabel content="Address Information" />
-          <ViewFormField
+          <ViewFormFieldWithSkelton isLoading={isLoading} 
             sm={6}
             heading="Street"
             param={contact?.address?.street && contact?.address?.street}
           />
-          <ViewFormField
+          <ViewFormFieldWithSkelton isLoading={isLoading} 
             sm={6}
             heading="Suburb"
             param={contact?.address?.suburb && contact?.address?.suburb}
           />
-          <ViewFormField
+          <ViewFormFieldWithSkelton isLoading={isLoading} 
             sm={6}
             heading="City"
             param={contact?.address?.city && contact?.address?.city}
           />
-          <ViewFormField
+          <ViewFormFieldWithSkelton isLoading={isLoading} 
             sm={6}
             heading="Region"
             param={contact?.address?.region && contact?.address?.region}
           />
-          <ViewFormField
+          <ViewFormFieldWithSkelton isLoading={isLoading} 
             sm={6}
             heading="Post Code"
             param={contact?.address?.postcode && contact?.address?.postcode}
           />
-          <ViewFormField
+          <ViewFormFieldWithSkelton isLoading={isLoading} 
             sm={6}
             heading="Country"
             param={contact?.address?.country && contact?.address?.country}
           />
-          <ViewFormField />
+          <ViewFormFieldWithSkelton isLoading={isLoading}  />
         </Grid>
         </DialogContent>
         <DialogLink onClose={handleConttactDialog}/>
--- src/pages/components/Dialog/CustomerDialog.js
+++ src/pages/components/Dialog/CustomerDialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 // import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,77 +8,80 @@ import { setCustomerDialog } from '../../../redux/slices/customer/customer';
 import { PATH_CUSTOMER } from '../../../routes/paths';
 import DialogLink from './DialogLink';
 import DialogLabel from './DialogLabel';
-import ViewFormField from '../ViewForms/ViewFormField';
 import FormLabel from '../DocumentForms/FormLabel';
+import ViewFormFieldWithSkelton from '../ViewForms/ViewFormFieldWithSkelton';
+import ViewFormField from '../ViewForms/ViewFormField';
 
 function CustomerDialog() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { customer, customerDialog } = useSelector((state) => state.customer);
+  const { customer, customerDialog, isLoading } = useSelector((state) => state.customer);
   const handleCustomerDialog = () => { dispatch(setCustomerDialog(false))  }
+
   return (
     <Dialog
       disableEnforceFocus
       maxWidth="lg"
       open={customerDialog}
       onClose={handleCustomerDialog}
-      keepMounted
       aria-describedby="alert-dialog-slide-description"
+      // keepMounted // for scroll lock
     >
-      <DialogLabel onClick={ handleCustomerDialog } content="Customer" />
+
+     <DialogLabel onClick={ handleCustomerDialog } content="Customer" />
       <DialogContent dividers>
-      <Grid item container sx={{ px: 2, pt: 2 }}>
-        <ViewFormField sm={12} heading="Name" param={customer?.name} />
-        <ViewFormField sm={12} heading="Trading Name" chips={customer?.tradingName} />
-        <ViewFormField sm={6} heading="Phone" param={customer?.mainSite?.phone} />
-        <ViewFormField sm={6} heading="Fax" param={customer?.mainSite?.fax} />
-        <ViewFormField sm={6} heading="Email" param={customer?.mainSite?.email} />
-        <ViewFormField sm={6} heading="Site Name" param={customer?.mainSite?.name} />
-        <FormLabel content="Address Information" />
-        <ViewFormField sm={6} heading="Street" param={customer?.mainSite?.address?.street} />
-        <ViewFormField sm={6} heading="Suburb" param={customer?.mainSite?.address?.suburb} />
-        <ViewFormField sm={6} heading="City" param={customer?.mainSite?.address?.city} />
-        <ViewFormField sm={6} heading="Region" param={customer?.mainSite?.address?.region} />
-        <ViewFormField sm={6} heading="Post Code" param={customer?.mainSite?.address?.postcode} />
-        <ViewFormField sm={12} heading="Country" param={customer?.mainSite?.address?.country} />
-        <ViewFormField
-          sm={6}
-          heading="Primary Biling Contact"
-          param={
-            customer?.primaryBillingContact &&
-            `${customer?.primaryBillingContact?.firstName} ${customer?.primaryBillingContact?.lastName}`
-          }
-        />
-        <ViewFormField
-          sm={6}
-          heading="Primary Technical Contact"
-          param={
-            customer?.primaryTechnicalContact &&
-            `${customer?.primaryTechnicalContact?.firstName} ${customer?.primaryTechnicalContact?.lastName}`
-          }
-        />
-      </Grid>
-      <Grid item container sx={{ px: 2, pb: 3 }}>
-        <FormLabel content="Howick Resources" />
-        <ViewFormField
-          sm={6}
-          heading="Account Manager"
-          param={customer?.accountManager?.firstName}
-          secondParam={customer?.accountManager?.lastName}
-        />
-        <ViewFormField
-          sm={6}
-          heading="Project Manager"
-          param={customer?.projectManager?.firstName}
-          secondParam={customer?.projectManager?.lastName}
-        />
-        <ViewFormField
-          sm={6}
-          heading="Suppport Manager"
-          param={customer?.supportManager?.firstName}
-          secondParam={customer?.supportManager?.lastName}
-        />
-      </Grid>
+        <Grid item container>
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={12} heading="Name" param={customer?.name} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={12} heading="Trading Name" chips={customer?.tradingName} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Phone" param={customer?.mainSite?.phone} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Fax" param={customer?.mainSite?.fax} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Email" param={customer?.mainSite?.email} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Site Name" param={customer?.mainSite?.name} />
+          <FormLabel content="Address Information" />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Street" param={customer?.mainSite?.address?.street} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Suburb" param={customer?.mainSite?.address?.suburb} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="City" param={customer?.mainSite?.address?.city} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Region" param={customer?.mainSite?.address?.region} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Post Code" param={customer?.mainSite?.address?.postcode} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={12} heading="Country" param={customer?.mainSite?.address?.country} />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Primary Biling Contact"
+            param={
+              customer?.primaryBillingContact &&
+              `${customer?.primaryBillingContact?.firstName} ${customer?.primaryBillingContact?.lastName}`
+            }
+          />
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Primary Technical Contact"
+            param={
+              customer?.primaryTechnicalContact &&
+              `${customer?.primaryTechnicalContact?.firstName} ${customer?.primaryTechnicalContact?.lastName}`
+            }
+          />
+        </Grid>
+        <Grid item container sx={{ px: 2, pb: 3 }}>
+          <FormLabel content="Howick Resources" />
+          
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Account Manager"
+            param={
+              customer?.accountManager &&
+              `${customer?.accountManager?.firstName} ${customer?.accountManager?.lastName}`
+            }
+          />
+
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Project Manager"
+            param={
+              customer?.projectManager &&
+              `${customer?.projectManager?.firstName} ${customer?.projectManager?.lastName}`
+            }
+          />
+
+          <ViewFormFieldWithSkelton isLoading={isLoading} sm={6} heading="Suppport Manager"
+            param={
+              customer?.supportManager &&
+              `${customer?.supportManager?.firstName} ${customer?.supportManager?.lastName}`
+            }
+          />
+
+        </Grid>
       </DialogContent>
       <DialogLink
         onClose={handleCustomerDialog}
