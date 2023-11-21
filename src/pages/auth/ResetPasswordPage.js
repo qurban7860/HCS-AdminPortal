b48219c3ee882edf946ec29b38533e3dbdead717
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-21 16:20:52 +1300

    reset password page update

--- src/pages/auth/ResetPasswordPage.js
+++ src/pages/auth/ResetPasswordPage.js
@@ -1,26 +1,36 @@
 // import { Helmet } from 'react-helmet-async';
 import { Link as RouterLink } from 'react-router-dom';
 // @mui
-import { Link, Typography } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 // routes
 import { PATH_AUTH } from '../../routes/paths';
 // components
 import Iconify from '../../components/iconify';
+import Logo from '../../components/logo';
 // sections
 import AuthResetPasswordForm from '../../sections/auth/AuthResetPasswordForm';
 // assets
 import { PasswordIcon } from '../../assets/icons';
+import { MotionContainer } from '../../components/animate';
 import { TITLES } from '../../constants/default-constants';
 
 // ----------------------------------------------------------------------
 
 export default function ResetPasswordPage() {
   return (
-    <>
-      {/* <Helmet>
-        <title> Reset Password Login | {CONFIG.APP_TITLE} </title>
-      </Helmet> */}
-      <PasswordIcon sx={{ mb: 5, height: 96 }} />
+   <MotionContainer>
+    <Box sx={{ position: 'relative'}}>
+      <Logo
+          width={450}
+          sx={{
+            margin: 'auto',
+            filter: 'grayscale(100%) opacity(30%)',
+            pointerEvents: 'none',
+            padding: '3rem 0'
+          }}
+        />
+        <PasswordIcon sx={{ position: 'absolute', width: '10%', top: '100px', right: '100px', transform: 'rotate(30deg)'}}/>
+      </Box>
       <Typography variant="h3" paragraph>
         {TITLES.FORGOT_PASSWORD}
       </Typography>
@@ -43,6 +53,6 @@ export default function ResetPasswordPage() {
         <Iconify icon="eva:chevron-left-fill" width={16} />
         {TITLES.FORGOT_RETURN}
       </Link>
-    </>
+    </MotionContainer>
   );
 }
