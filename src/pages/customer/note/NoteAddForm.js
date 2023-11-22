1f1dbaa3f031bfebca2273b0447c9a324c7ef59b
Ali Hassan <a.hassan@terminustech.com>
2023-11-22 15:41:34 +0500

    Revert "Revert "changes revised on interfaces""
    This reverts commit 8898b5062918a14b89cd28f54a11de24305aba91.

--- src/pages/customer/note/NoteAddForm.js
+++ src/pages/customer/note/NoteAddForm.js
@@ -103,7 +103,7 @@ export default function NoteAddForm() {
                   // freeSolo
                   value={siteVal || null}
                   options={activeSites}
-                  isOptionEqualToValue={(option, value) => option.name === value.name}
+                  isOptionEqualToValue={(option, value) => option?._id === value?._id}
                   getOptionLabel={(option) => `${option.name ? option.name : ''}`}
                   onChange={(event, newValue) => {
                     if (newValue) {
@@ -126,7 +126,7 @@ export default function NoteAddForm() {
                   // freeSolo
                   value={contactVal || null}
                   options={activeContacts}
-                  isOptionEqualToValue={(option, value) => option.firstName === value.firstName}
+                  isOptionEqualToValue={(option, value) => option?._id === value?._id }
                   getOptionLabel={(option) =>
                     `${option.firstName ? option.firstName : ''} ${
                       option.lastName ? option.lastName : ''
