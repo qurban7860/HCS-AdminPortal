d0e84d211caedbdc4386a0e46babb911eb7db317
Ali Hassan <a.hassan@terminustech.com>
2024-01-05 11:10:33 +0500

    INI compare page integrate

---
+++ src/pages/machine/Historical Configurations/HistoricalConfigurationsCompareViewForm.js
@@ -0,0 +1,59 @@
+import { useMemo, memo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// @mui
+import {  Card, Grid, Stack, Skeleton } from '@mui/material';
+import JsonEditor from './JsonEditorMerge';
+// redux
+import { setHistoricalConfigurationCompareViewFormVisibility } from '../../../redux/slices/products/historicalConfiguration';
+// components
+import ViewFormAudit from '../../components/ViewForms/ViewFormAudit';
+import ViewFormEditDeleteButtons from '../../components/ViewForms/ViewFormEditDeleteButtons';
+
+
+function HistoricalConfigurationsViewForm() {
+
+  const { historicalConfiguration, isLoading } = useSelector((state) => state.historicalConfiguration);
+
+  const dispatch = useDispatch();
+
+  const defaultValues = useMemo(
+    () => ({
+      isActive:                             historicalConfiguration?.isActive,
+      createdAt:                            historicalConfiguration?.createdAt || '',
+      createdByFullName:                    historicalConfiguration?.createdBy?.name || '',
+      createdIP:                            historicalConfiguration?.createdIP || '',
+      updatedAt:                            historicalConfiguration?.updatedAt || '',
+      updatedByFullName:                    historicalConfiguration?.updatedBy?.name || '',
+      updatedIP:                            historicalConfiguration?.updatedIP || '',
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [ historicalConfiguration]
+  );
+  
+  return (
+    <Card sx={{ p: 2 }}>
+      <Grid>
+        <ViewFormEditDeleteButtons backLink={()=> dispatch(setHistoricalConfigurationCompareViewFormVisibility(false))} />
+        <Stack spacing={2} sx={{p:2}}>
+          {isLoading ? 
+          <>
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton animation="wave" />
+            <Skeleton animation="wave" />
+            <Skeleton animation="wave" />
+            <Skeleton animation={false} />
+            <Skeleton animation={false} />
+            <Skeleton animation={false} />
+          </>
+           :  <JsonEditor  value={ historicalConfiguration } modifiedValue={ historicalConfiguration } readOnly />  
+           }
+        </Stack>
+          <ViewFormAudit  defaultValues={defaultValues} />
+      </Grid>
+    </Card>
+  );
+}
+
+export default memo(HistoricalConfigurationsViewForm)
