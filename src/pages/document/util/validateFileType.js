05ee9b4864338c71fbab2ead4a9fe806f0ce719d
Ali Hassan <a.hassan@terminustech.com>
2024-01-09 15:03:48 +0500

    document file upload revised

--- src/pages/document/util/validateFileType.js
+++ src/pages/document/util/validateFileType.js
@@ -3,7 +3,7 @@ import { Snacks, allowedExtensions } from '../../../constants/document-constants
 const validateFileType = (value, options) => {
   const { path, createError } = options;
   if (value && Array.isArray(value)) {
-    if (value.length > 10) {
+    if (value.length > 20) {
       return createError({
         message: Snacks.fileMaxCount,
         path,
