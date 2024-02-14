ff28944bbf630b26858a8c3282bdb06688115dc3
Ali Hassan <a.hassan@terminustech.com>
2024-02-14 14:22:24 +0500

    MAX_UPLOAD_FILES defined systematically

--- src/pages/document/util/validateFileType.js
+++ src/pages/document/util/validateFileType.js
@@ -1,9 +1,11 @@
 import { Snacks, allowedExtensions } from '../../../constants/document-constants';
 
+const maxFiles = JSON.parse( localStorage.getItem('configurations'))?.find( ( c )=> c?.name === 'MAX_UPLOAD_FILES' )
+
 const validateFileType = (value, options) => {
   const { path, createError } = options;
   if (value && Array.isArray(value)) {
-    if (value.length > 200) {
+    if (value.length > ( Number(maxFiles?.value) || 20 ) ) {
       return createError({
         message: Snacks.fileMaxCount,
         path,
