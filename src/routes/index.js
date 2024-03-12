fb05afc05caee3f58f86b3213e33aa5a7064d17b
Ali Hassan <a.hassan@terminustech.com>
2024-03-12 14:18:05 +0500

    path after login revised

--- src/routes/index.js
+++ src/routes/index.js
@@ -308,7 +308,6 @@ export default function Router() {
         { path: 'machineByModels', element: <MachineByModelsViewForm /> },
         { path: 'machineByYears', element: <MachineByYearsViewForm /> },
         { path: 'permission-denied', element: <PermissionDeniedPage /> },
-        { path: 'blank', element: <BlankPage /> },
       ],
     },
     {
