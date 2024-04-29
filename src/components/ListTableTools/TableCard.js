ef714eef65413ece9a117706ad0a4764b5b203c0
Ali Hassan <a.hassan@terminustech.com>
2024-04-29 10:36:16 +0500

    changes revised in the below components

--- src/components/ListTableTools/TableCard.js
+++ src/components/ListTableTools/TableCard.js
@@ -3,7 +3,7 @@ import { Card } from '@mui/material'
 import PropTypes from 'prop-types';
 
 const TableCard = ({children}) => 
-    <Card sx={{ my: 3 }} >
+    <Card sx={{ mt: 3 }} >
         {children}
     </Card>
 
