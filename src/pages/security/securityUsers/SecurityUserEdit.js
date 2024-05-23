23e706a28ba924420df05027eb1e9f5cd549d736
Ali Hassan <a.hassan@terminustech.com>
2024-05-23 10:59:19 +0500

    security user edit form revised

--- src/pages/security/securityUsers/SecurityUserEdit.js
+++ src/pages/security/securityUsers/SecurityUserEdit.js
@@ -1,20 +1,33 @@
 // @mui
+import { useLayoutEffect } from 'react';
 import { Container, Card } from '@mui/material';
 // redux
-import UserEditForm from './SecurityUserEditForm';
+import { useDispatch } from 'react-redux';
+import {  useParams } from 'react-router-dom';
+import SecurityUserEditForm from './SecurityUserEditForm';
 import { Cover } from '../../../components/Defaults/Cover';
 import { StyledCardContainer } from '../../../theme/styles/default-styles';
+import { getSecurityUser } from '../../../redux/slices/securityUser/securityUser';
 
 // ----------------------------------------------------------------------
 
 export default function SecurityUserEdit() {
+
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  
+  useLayoutEffect(() => {
+    if (id) {
+      dispatch(getSecurityUser(id));
+    }
+  },[dispatch, id]);
 
   return (
     <Container maxWidth={false}>
       <StyledCardContainer>
         <Cover name="Edit User"/>
       </StyledCardContainer>
-      <UserEditForm />
+      <SecurityUserEditForm />
     </Container>
   );
 }
