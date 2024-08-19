f6c1b4fc810d92f0f7fcebdcd339c011c0eab0ba
Ali Hassan <a.hassan.dev@outlook.com>
2024-08-19 12:35:25 +0500

    document actions disabled upon archived machine

--- src/pages/machine/drawings/DrawingView.js
+++ src/pages/machine/drawings/DrawingView.js
@@ -1,13 +1,14 @@
 import { Container } from '@mui/material';
+import { useSelector } from 'react-redux';
 import DocumentHistoryViewForm from '../../documents/DocumentHistoryViewForm'
 import MachineTabContainer from '../util/MachineTabContainer';
 
 export default function DrawingView() {
-
+  const { machine } = useSelector((state) => state.machine);
   return (
     <Container maxWidth={false}>
       <MachineTabContainer currentTabValue='drawings' />
-      <DocumentHistoryViewForm machineDrawingPage />
+      <DocumentHistoryViewForm machineDrawingPage allowActions={ machine?.isArchived } />
     </Container>
     )
 }
