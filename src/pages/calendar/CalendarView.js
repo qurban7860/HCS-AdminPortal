d303c12d38ebc1d66236c77c13c8523ffbd6ec1f
Ali Hassan <a.hassan.dev@outlook.com>
2024-08-28 15:03:55 +0500

    changes revised

--- src/pages/calendar/CalendarView.js
+++ src/pages/calendar/CalendarView.js
@@ -1,7 +1,9 @@
-import { useLayoutEffect, useState } from 'react' 
+import { useLayoutEffect, useState, useRef } from 'react' 
 import { useDispatch, useSelector } from 'react-redux';
 import CalendarPage from './CalendarPage'
 import { useAuthContext } from '../../auth/useAuthContext';
+// hooks
+import useResponsive from '../../hooks/useResponsive';
 import { getEvents, setEventModel } from '../../redux/slices/event/event';
 import { getActiveCustomers } from '../../redux/slices/customer/customer';
 import { getActiveSPContacts } from '../../redux/slices/customer/contact';
@@ -9,11 +11,17 @@ import { getActiveSecurityUsers, getSecurityUser } from '../../redux/slices/secu
 
 export default function CalendarView() {
     const dispatch = useDispatch();
-    const { userId } = useAuthContext();
+    const calendarRef = useRef(null);
+    const isDesktop = useResponsive('up', 'sm');
+    const { userId, isAllAccessAllowed } = useAuthContext();
     const { activeSpContacts } = useSelector((state) => state.contact);
+    const { securityUser } = useSelector((state) => state.user);
     const [ date, setDate ] = useState(new Date());
     const [ previousDate, setPreviousDate ] = useState(null);
     const [ defaultNotifyContacts, setDefaultNotifyContacts ] = useState([]);
+    const [ selectedContact, setSelectedContact ] = useState(null);
+    const [ selectedUser, setSelectedUser ] = useState(null);
+    const [ view, setView ] = useState('dayGridMonth');
 
     useLayoutEffect(() => {
         const fetchData = async () => {
@@ -40,23 +48,48 @@ export default function CalendarView() {
     }, [dispatch, date, previousDate, userId]);
 
     useLayoutEffect(() => {
-      const configurations = JSON.parse(localStorage.getItem('configurations'));
-      const def_contacts = configurations?.find(c => c.name === 'Default_Notify_Contacts');
-      const emails = def_contacts?.value?.split(',')?.map(c => c.trim().toLowerCase());
-      if (emails && activeSpContacts) {
-          const filteredContacts = activeSpContacts.filter(_contact => 
-              emails.includes(_contact.email?.trim().toLowerCase())
-          );
-          setDefaultNotifyContacts(filteredContacts);
-      }
+        const configurations = JSON.parse(localStorage.getItem('configurations'));
+        const def_contacts = configurations?.find(c => c.name === 'Default_Notify_Contacts');
+        const emails = def_contacts?.value?.split(',')?.map(c => c.trim().toLowerCase());
+        if (emails && activeSpContacts) {
+                const filteredContacts = activeSpContacts.filter(_contact => 
+                    emails.includes(_contact.email?.trim().toLowerCase())
+                );
+            setDefaultNotifyContacts(filteredContacts);
+        }
     }, [ activeSpContacts ]);
+
+    useLayoutEffect(()=>{
+        if(!isAllAccessAllowed){
+            setSelectedUser(securityUser);
+            setSelectedContact(securityUser?.contact)
+        }
+    },[ securityUser, isAllAccessAllowed ])
+    
+    
+    useLayoutEffect(() => {
+        const calendarEl = calendarRef.current;
+        if (calendarEl) {
+            const calendarApi = calendarEl.getApi();
+            const newView = isDesktop ? 'dayGridMonth' : 'listWeek';
+            calendarApi.changeView(newView);
+            setView(newView);
+        }
+    }, [ isDesktop ]);
 
     return (
         <CalendarPage 
+            calendarRef={calendarRef}
             date={date}  
-            setDate={setDate} 
+            view={view}
+            selectedUser={selectedUser}
             previousDate={previousDate} 
+            selectedContact={selectedContact}
+            setDate={setDate} 
+            setView={setView}
             setPreviousDate={setPreviousDate} 
+            setSelectedUser={setSelectedUser}
+            setSelectedContact={setSelectedContact}
             defaultNotifyContacts={defaultNotifyContacts}
         />
     );
