541368d55755915249149495d16b79eefcf466a8
Qurban Hanif <qurbanhanif120@gmail.com>
2024-09-20 19:42:21 +0500

    hide archived log in machine log

--- src/pages/machine/logs/MachineLogsListTableToolbar.js
+++ src/pages/machine/logs/MachineLogsListTableToolbar.js
@@ -64,37 +64,48 @@ export default function MachineLogsListTableToolbar({
 // </Button>
 
   return (
-    <Stack {...options} direction="column" spacing={1} sx={{ px: 2.5, py: 3, pt: 1.5}}>
-      {!archivedLogs ? null
-      : (
-        <Stack direction="row" spacing={1} sx={{ alignSelf: 'flex-start', alignItems: 'center' }}>
-          <IconTooltip
-            title='Back'
-            onClick={toggleArchivedLogs}
-            color={theme.palette.primary.main}
-            icon="mdi:arrow-left"
-          />
-          <Divider orientation="vertical" flexItem />
-          <Box sx={{ borderBottom: 2, borderColor: 'primary.main', pb: 1 }}>
-            <Typography variant="h5" color="text.primary">Archived Logs</Typography>
-          </Box>
-        </Stack>
-      )}
-     </>
+    <Stack {...options} direction="column" spacing={1} sx={{ px: 2.5, py: 3, pt: 1.5 }}>
+      {location.pathname !== PATH_MACHINE_LOGS.root && (
+        <>
+          {!archivedLogs ? null : (
+            <Stack
+              direction="row"
+              spacing={1}
+              sx={{ alignSelf: 'flex-start', alignItems: 'center' }}
+            >
+              <IconTooltip
+                title="Back"
+                onClick={toggleArchivedLogs}
+                color={theme.palette.primary.main}
+                icon="mdi:arrow-left"
+              />
+              <Divider orientation="vertical" flexItem />
+              <Box sx={{ borderBottom: 2, borderColor: 'primary.main', pb: 1 }}>
+                <Typography variant="h5" color="text.primary">
+                  Archived Logs
+                </Typography>
+              </Box>
+            </Stack>
+          )}
+        </>
       )}
       <SearchBarCombo
-        isFiltered={ isFiltered }
-        value={ filterName }
-        onChange={ onFilterName }
-        onClick={ onResetFilter }
-        logTypes={ location.pathname !== PATH_MACHINE_LOGS.root ? logTypes : undefined }
-        SubOnClick={ toggleAdd }
-        dateFrom={ dateFrom }
-        dateTo={ dateTo }
+        isFiltered={isFiltered}
+        value={filterName}
+        onChange={onFilterName}
+        onClick={onResetFilter}
+        logTypes={location.pathname !== PATH_MACHINE_LOGS.root ? logTypes : undefined}
+        SubOnClick={toggleAdd}
+        dateFrom={dateFrom}
+        dateTo={dateTo}
         isDateFromDateTo
         // openGraph={ location.pathname !== PATH_MACHINE_LOGS.root ? toggleGraph : undefined }
-        addButton={!(machine?.isArchived || isHistory) && location.pathname !== PATH_MACHINE_LOGS.root? BUTTONS.ADD_MACHINE_LOGS : undefined}
-        transferredMachine={ machine?.status?.slug==='transferred' }
+        addButton={
+          !(machine?.isArchived || isHistory) && location.pathname !== PATH_MACHINE_LOGS.root
+            ? BUTTONS.ADD_MACHINE_LOGS
+            : undefined
+        }
+        transferredMachine={machine?.status?.slug === 'transferred'}
       />
     </Stack>
   );
