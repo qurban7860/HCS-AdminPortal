ae11e23aa352b34dd31338aa9f4a1eb3de4002ac
Qurban Hanif <qurbanhanif120@gmail.com>
2024-10-02 14:02:59 +0500

    set width of setting history

--- src/components/ViewForms/ViewFormMachineSettingHistoryMenuPopover.js
+++ src/components/ViewForms/ViewFormMachineSettingHistoryMenuPopover.js
@@ -7,8 +7,8 @@ import { fDateTime } from '../../utils/formatTime';
 
 function ViewFormMachineSettingHistoryMenuPopover({ open, onClose, ListArr, ListTitle }) {
   return (
-    <MenuPopover open={open} onClose={onClose} sx={{ p: 0 }}>
-      <Box sx={{ py: 1, px: 1 }} alignContent="center" alignItems="center" >
+    <MenuPopover open={open} onClose={onClose} sx={{ p: 0, width: { xs: '90vw', sm: '70vw', md: '50vw', lg: '40vw' } }}> 
+      <Box sx={{ py: 1, px: 1, width: '100%' }} alignContent="center" alignItems="center" >
         <FormLabel content={ListTitle} />
         <TableContainer component={Paper} >
           <Table size="small" aria-label="a dense table" >
@@ -22,7 +22,7 @@ function ViewFormMachineSettingHistoryMenuPopover({ open, onClose, ListArr, List
             <TableBody>
               {ListArr?.map((m) => (
                 <TableRow key={m?._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
-                  <TableCell align='left'>{m?.techParamValue || ''}</TableCell>
+                  <TableCell align='left'  sx={{ wordBreak: 'break-word', whiteSpace: 'normal', maxWidth: 200 }}>{m?.techParamValue || ''}</TableCell>
                   <TableCell align='left'>{m?.updatedBy?.name || ''}</TableCell>
                   <TableCell align='right'>{fDateTime(m?.updatedAt) || ''}</TableCell>
                 </TableRow>
@@ -34,6 +34,7 @@ function ViewFormMachineSettingHistoryMenuPopover({ open, onClose, ListArr, List
     </MenuPopover>
   );
 }
+
 
 export default memo(ViewFormMachineSettingHistoryMenuPopover);
 
