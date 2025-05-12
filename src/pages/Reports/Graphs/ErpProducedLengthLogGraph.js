6997609e57693992bdab1cae5a4dd072955b1028
Qurban Hanif <qurbanhanif120@gmail.com>
2025-05-12 18:53:48 +0500

    set hourly format in erp graph

--- src/pages/Reports/Graphs/ErpProducedLengthLogGraph.js
+++ src/pages/Reports/Graphs/ErpProducedLengthLogGraph.js
@@ -32,8 +32,10 @@ const ErpProducedLengthLogGraph = ({ timePeriod, customer, graphLabels }) => {
         labels = Array.from({ length: 24 }, (_, i) => {
           const date = new Date();
           date.setHours(date.getHours() - i);
+          const month = String(date.getMonth() + 1).padStart(2, '0');
+          const day = String(date.getDate()).padStart(2, '0');
           const hour = String(date.getHours()).padStart(2, '0');
-          return `${hour}:00`;
+           return `${month}/${day} ${hour}`;
         }).reverse();                   
         break;
       case 'Daily':
