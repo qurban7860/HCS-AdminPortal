e212d4b0217161922c2df09a1555eaf7117e4aa2
Qurban Hanif <qurbanhanif120@gmail.com>
2025-05-29 15:03:28 +0500

    update UI and changes revised in dates of Machine Life Cycle

--- src/pages/machine/machineLifecycle/MachineLifecycle.js
+++ src/pages/machine/machineLifecycle/MachineLifecycle.js
@@ -12,7 +12,6 @@ import {
 import EventIcon from '@mui/icons-material/Event';
 import { useSelector, useDispatch } from 'react-redux';
 
-import ViewFormAudit from '../../../components/ViewForms/ViewFormAudit';
 import MachineTabContainer from '../util/MachineTabContainer';
 import { FORMLABELS } from '../../../constants/document-constants';
 import FormLabel from '../../../components/DocumentForms/FormLabel';
@@ -28,73 +27,24 @@ const MachineLifecycle = () => {
       dispatch(getMachineLifeCycle(machine._id));
     }
   }, [dispatch, machine?._id]);
-  
-  const defaultValues = useMemo(
-    () => ({
-      createdBy: machineLifeCycle?.createdBy?.name || '',
-      updatedBy: machineLifeCycle?.updatedBy?.name || '',
-      createdAt: machineLifeCycle?.createdAt || '',
-      updatedAt: machineLifeCycle?.updatedAt || '',
-      createdByFullName: machineLifeCycle?.createdBy?.name || '',
-      updatedByFullName: machineLifeCycle?.updatedBy?.name || '',
-    }),
-    [machineLifeCycle]
-  );
-  
+
   const machineActions = useMemo(() => {
     const actions = [];
-  
-    if (Array.isArray(machineLifeCycle?.transferredHistory)) {
-      machineLifeCycle.transferredHistory.forEach((history) => {
-        if (history.manufactureDate) {
-          actions.push({ sortDate: history.manufactureDate, action: `Manufacture for ${history?.customer?.name || 'N/A'}` });
-        }
-  
-        if (history.installationDate) {
-          actions.push({ sortDate: history.installationDate, action: `Installation at ${history?.customer?.name || 'N/A'}` });
-        }
-  
-        if (history.purchaseDate) {
-          actions.push({ sortDate: history.purchaseDate, action: `Purchase by ${history?.customer?.name || 'N/A'}` });
-        }
-  
-        if (history.shippingDate) {
-          actions.push({ sortDate: history.shippingDate, action: `Shipping to ${history?.customer?.name || 'N/A'}` });
-        }
-  
-        if (history.transferredDate) {
-          actions.push({ sortDate: history.transferredDate, action: `Machine Transfer` });
-        }
-  
-        if (history.supportExpireDate) {
-          actions.push({ sortDate: history.supportExpireDate, action: `Support expire for ${history?.customer?.name || 'N/A'}` });
-        }
-  
-        if (history.decommissionedDate) {
-          actions.push({ sortDate: history.decommissionedDate, action: `Decommissioned at ${history?.customer?.name || 'N/A'}` });
-        }
+    const currentDate = new Date();
 
-      });
-    }
-    
-    if (Array.isArray(machineLifeCycle?.portalKey)) {
-      machineLifeCycle.portalKey.forEach((key) => {
-        if (key.createdAt) {
-          actions.push({ sortDate: key.createdAt, action: `Portal Connection by ${key?.createdBy?.name || 'N/A'}` });
+    if (Array.isArray(machineLifeCycle)) {
+      machineLifeCycle.forEach((item) => {
+        if (item.date && item.type) {
+          const eventDate = new Date(item.date);
+          if (eventDate <= currentDate) {
+            const actionDescription = item.type;
+            actions.push({ sortDate: item.date, action: actionDescription });
+          }
         }
       });
     }
 
-    if (Array.isArray(machineLifeCycle?.serviceReports)) {
-      machineLifeCycle.serviceReports.forEach((report) => {
-        if (report.serviceDate) {
-          actions.push({ sortDate: report.serviceDate, action: `Service Report` });
-        }
-      });
-    }
-  
-    return actions
-    .sort((a, b) => new Date(b.sortDate) - new Date(a.sortDate));
+    return actions.sort((a, b) => new Date(b.sortDate) - new Date(a.sortDate));
   }, [machineLifeCycle]);
 
   return (
@@ -103,7 +53,15 @@ const MachineLifecycle = () => {
       <Card sx={{ p: 3 }}>
         <Box>
           {/* Title */}
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: 2, flexWrap: 'wrap' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              mb: 2,
+              flexWrap: 'wrap',
+            }}
+          >
             <FormLabel content={FORMLABELS.MACHINE_LIFECYCLE} />
           </Box>
 
@@ -113,11 +71,9 @@ const MachineLifecycle = () => {
               position: 'relative',
               display: 'flex',
               flexDirection: 'column',
-              alignItems: 'center',
+              alignItems: 'flex-start', 
               mb: 6,
-              width: { xs: '100%', md: '50%' },
-              mr: { xs: 0, md: '30%' },
-              ml: { xs: 0, md: 'auto' },
+              width: '100%',
             }}
           >
             {/* Vertical Line */}
@@ -126,8 +82,7 @@ const MachineLifecycle = () => {
                 position: 'absolute',
                 top: 20,
                 bottom: 20,
-                left: '35%',
-                transform: 'translateX(-30%)',
+                left: 150, 
                 width: { xs: 2, md: 4 },
                 bgcolor: 'primary.main',
                 zIndex: 0,
@@ -142,21 +97,45 @@ const MachineLifecycle = () => {
             ) : (
               machineActions.map((item, index) => (
                 <Fade in timeout={500} key={index}>
-                  <Box sx={{ display: 'flex', alignItems: 'center', width: '100%', mb: 3, position: 'relative', zIndex: 1 }}>
-                    <Box sx={{ flex: 1, textAlign: 'right', pr: 2 }}>
-                      <Typography variant="body2" fontWeight="bold" color="text.secondary">
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      width: '100%',
+                      mb: 3,
+                      position: 'relative',
+                      zIndex: 1,
+                      pl: 4,
+                    }}
+                  >
+                    <Box sx={{ width: 100 }}>
+                      <Typography
+                        variant="body2"
+                        fontWeight="bold"
+                        color="text.secondary"
+                      >
                         {item.sortDate ? fDate(item.sortDate) : 'N/A'}
                       </Typography>
                     </Box>
 
-                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        mr: 2,
+                      }}
+                    >
                       <Avatar sx={{ bgcolor: 'primary.main', width: 42, height: 40 }}>
                         <EventIcon sx={{ fontSize: 24 }} />
                       </Avatar>
                     </Box>
 
-                    <Box sx={{ flex: 2, pl: 2 }}>
-                      <Paper elevation={4} sx={{ p: 2, backgroundColor: '#fff', borderRadius: 2 }}>
+                    <Box sx={{ flex: 1 }}>
+                      <Paper
+                        elevation={4}
+                        sx={{ p: 2, backgroundColor: '#fff', borderRadius: 2 }}
+                      >
                         <Typography variant="h6" fontWeight={600}>
                           {item.action}
                         </Typography>
@@ -170,7 +149,6 @@ const MachineLifecycle = () => {
 
           <Divider sx={{ mb: 1, mt: -2 }} />
         </Box>
-        <ViewFormAudit defaultValues={defaultValues} />
       </Card>
     </Container>
   );
