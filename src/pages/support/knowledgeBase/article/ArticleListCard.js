e45d08dad2c15c1ae757add9ae2bf8819da81988
Muhammad Zeeshan Aslam <zamrana6768@gmail.com>
2025-05-30 11:27:47 +0500

    fixes

--- src/pages/support/knowledgeBase/article/ArticleListCard.js
+++ src/pages/support/knowledgeBase/article/ArticleListCard.js
@@ -1,18 +1,23 @@
 import PropTypes from 'prop-types';
 // @mui
 import {
+  Button,
   Card,
   CardHeader,
   Grid,
+  Stack,
+  Typography,
   useTheme,
 } from '@mui/material';
 import CardActions from '@mui/material/CardActions';
 // utils
 import { fDate } from '../../../../utils/formatTime';
 import { ICONS } from '../../../../constants/icons/default-icons';
-import ArticleInfo from './ArticleInfo';
 import { PATH_SUPPORT } from '../../../../routes/paths';
 import OpenInNewPage from '../../../../components/Icons/OpenInNewPage';
+import IconTooltip from '../../../../components/Icons/IconTooltip';
+import { articleStatusOptions } from '../../../../utils/constants';
+import Iconify from '../../../../components/iconify';
 // ----------------------------------------------------------------------
 
 ArticleListCard.propTypes = {
@@ -22,7 +27,6 @@ ArticleListCard.propTypes = {
   onViewRow: PropTypes.func,
   prefix: PropTypes.string,
 };
-
 
 export default function ArticleListCard({
   row,
@@ -42,26 +46,52 @@ export default function ArticleListCard({
 
   const theme = useTheme();
 
+  const statusOption = articleStatusOptions.find((option) => option.value === status);
+    
+
   const renderAritcle = (
     <Card sx={{ cursor: 'pointer', '&:hover': { boxShadow: theme.customShadows.z24 } }} onClick={onViewRow}>
       <CardHeader sx={{ pt: 1, px: 2 }} 
-          title={<>{title}<OpenInNewPage onClick={handleExternalLink} /></>} 
+          title={<>
+                  {title}
+                  <IconTooltip title='Open in New Tab' icon='fluent:open-12-regular' onClick={handleExternalLink} iconSx={{ border: 'none', width: '30px' }} />
+              </>} 
+          action={
+            <>
+              <IconTooltip 
+                title={statusOption?.label} 
+                icon={statusOption?.icon} 
+                color={statusOption?.color} 
+                onClick={(event) => event.stopPropagation()} 
+                iconSx={{ border: 'none' }} 
+              />
+              <IconTooltip 
+                onClick={(event) => event.stopPropagation()}
+                title={customerAccess ? ICONS.ALLOWED.heading : ICONS.DISALLOWED.heading} 
+                color={customerAccess ? ICONS.ALLOWED.color : ICONS.DISALLOWED.color}
+                icon={customerAccess ? ICONS.ALLOWED.icon : ICONS.DISALLOWED.icon}
+                iconSx={{ border: 'none' }}
+              />
+              <IconTooltip
+                onClick={(event) => event.stopPropagation()}
+                title={isActive ? ICONS.ACTIVE.heading : ICONS.INACTIVE.heading} 
+                color={isActive ? ICONS.ACTIVE.color : ICONS.INACTIVE.color}
+                icon={isActive ? ICONS.ACTIVE.icon : ICONS.INACTIVE.icon}
+                iconSx={{ border: 'none' }}
+              />
+            </>
+          }
           />
-      <CardActions sx={{ pt: 1, px: 2 }}>
-        <Grid container spacing={2} direction="row" justifyContent="space-between">
-          <Grid item display="flex" gap={1}>
-            <ArticleInfo label={`${prefix}-${articleNo} (${category?.name})`} />
-            <ArticleInfo label={status} />
-            <ArticleInfo color={customerAccess ? ICONS.ALLOWED.color : ICONS.DISALLOWED.color} icon={customerAccess ? ICONS.ALLOWED.icon : ICONS.DISALLOWED.icon} tooltip={customerAccess ? ICONS.ALLOWED.heading : ICONS.DISALLOWED.heading} />
-            <ArticleInfo color={isActive ? ICONS.ACTIVE.color : ICONS.INACTIVE.color} icon={isActive ? ICONS.ACTIVE.icon : ICONS.INACTIVE.icon} tooltip={isActive ? ICONS.ACTIVE.heading : ICONS.INACTIVE.heading} />
-          </Grid>
-          <Grid item display="flex" gap={1}>
-            <ArticleInfo icon='mdi:clock-outline' label={fDate(updatedAt)} tooltip='Updated At' />
-          </Grid>
-        </Grid>
+      <CardActions sx={{ px: 2, pt:0 }}>
+        <Stack direction="row" spacing={2} justifyContent="space-between" width="100%">
+          <Typography variant="caption" sx={{ fontStyle: 'italic', color: 'text.disabled' }}>{`${prefix}-${articleNo} (${category?.name})`}</Typography>
+          <Typography variant="caption" sx={{ fontStyle: 'italic', color: 'text.disabled' }}>Updated At: {fDate(updatedAt)}</Typography>
+        </Stack>
       </CardActions>
     </Card>
   );
+
+  
 
   return (renderAritcle);
 }
